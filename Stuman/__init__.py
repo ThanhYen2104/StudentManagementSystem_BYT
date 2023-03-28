58fd493bdb8b45c1567df10b0c84619150986b05
ThanhYen2104 <thanhyen0421@gmail.com>
2023-03-28 14:26:31 +0700

    Change import Admin

--- Stuman/__init__.py
+++ Stuman/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/stumansys?charset=utf8mb4"\
--- Stuman/index.py
+++ Stuman/index.py
@@ -1,6 +1,5 @@
 from flask import render_template
 from Stuman import app
-from flask_admin import Admin
 
 admin = Admin(app, name="BYT EDUCATION ADMINISTRATION", template_mode='bootstap4')
 
