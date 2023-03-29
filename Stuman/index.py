ecf5199b317c69d1e061e2ba3e572a9a20b11caa
ThanhYen2104 <thanhyen0421@gmail.com>
2023-03-29 13:45:57 +0700

    Change Branch and name import from

--- Stuman/index.py
+++ Stuman/index.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from Stuman import app
+from StudentManagementSystem_BYT.Stuman import app
 from flask_admin import Admin
 
 admin = Admin(app, name="BYT EDUCATION ADMINISTRATION", template_mode='bootstap4')
