7e24682da13804ccf4e64c0177f507eada4b7b38
ThanhYen2104 <thanhyen0421@gmail.com>
2023-04-06 15:33:46 +0700

    Find student and display

--- Stuman/index.py
+++ Stuman/index.py
@@ -6,8 +6,9 @@ from StudentManagementSystem_BYT.Stuman import app, models
 def home():
     grade = models.get_grade()
     grade_id = request.args.get('grade_id')
+    classes_id = request.args.get('classes_id')
     kw = request.args.get('kw')
-    student = models.get_students(kw=kw, grade_id=grade_id)
+    student = models.get_students(kw=kw, grade_id=grade_id, classes_id=classes_id)
     return render_template('index.html', grade=grade, student=student)
 
 
@@ -17,9 +18,15 @@ def man_Student():
     return render_template("student.html", students=students)
 
 
+@app.route("/student/<int:student_id>")
+def student_info(student_id):
+    student_id = models.get_student_by_id(student_id)
+
+
 @app.route("/class")
 def man_Class():
-    return render_template("classes.html")
+    students = models.get_students()
+    return render_template("classes.html", students=students)
 
 
 @app.route("/subject")
